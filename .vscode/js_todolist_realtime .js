3f8fe88144705c5d285bd1e9adbc95312fde2116
包裕群 <boyu019@gmai.com>
2023-12-21 20:41:03 +0800

    登入 UI 與動作綁定

---
+++ .vscode/js_todolist_realtime .js
@@ -0,0 +1,88 @@
+import { TodoRealtime } from './class/TodoRealtime.js';
+// import { UID } from './class/UID.js';
+import { App } from './Firebase/App.js';
+import { Auth } from './Firebase/Auth.js';
+
+const app = await App.init();
+const auth = new Auth(app);
+
+let todoApp = document.querySelector('#todo-app');
+let uidApp = document.querySelector('#uid-app');
+
+let email = 'kindping@gmail.com'
+let password = '12345678'
+
+// let user = await auth.register(email, password);
+// let user = await auth.signIn(email, password);
+// console.log(user);
+
+const authed = (user) => {
+    let uid = user.uid;
+    todoApp.classList.add('active');
+    // TODO Application.
+    let elInput = document.querySelector('#todo-in');
+    let elAddBtn = document.querySelector('#todo-add-btn');
+    let elItem = document.querySelector('#todo-item');
+    let elChangeUid = document.querySelector('#change-uid-btn');
+    let todo = new TodoRealtime(elItem, uid);
+
+    const addTodo = () => {
+        let value = elInput.value;
+        if (!value) {
+            elInput.focus();
+            return; // void
+        }
+
+        elInput.value = '';
+        elInput.focus();
+
+        todo.add(value);
+        todo.render();
+    }
+
+    elAddBtn.addEventListener('click', (e) => {
+        addTodo();
+    })
+
+
+    elInput.addEventListener('keyup', (e) => {
+        if (e.key.toString().toUpperCase() == 'ENTER') {
+            addTodo();
+        }
+    })
+
+    elChangeUid.addEventListener('click', async (e) => {
+        e.preventDefault();
+        await auth.signOut()
+        location.reload();
+    })
+
+
+    let elCurentUid = document.querySelector('#current-uid');
+    elCurentUid.innerHTML = uid
+}
+
+const unauthed = () => {
+    uidApp.classList.add('active');
+    let elAccount = document.querySelector('#todo-account');
+    let elPassword = document.querySelector('#todo-password');
+
+    let elSignInBtn = document.querySelector('#todo-signin-btn')
+    elSignInBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let account = elAccount.value;
+        let password = elPassword.value;
+        console.log(account, password)
+    })
+
+    // let elUid = document.querySelector('#todo-uid');
+    // let elBtn = document.querySelector('#todo-uid-btn');
+    // elBtn.addEventListener('click', (e) => {
+    //     let value = elUid.value;
+    //     if (value) {
+    //         location.reload();
+    //     }
+    // })
+}
+
+auth.onChange(authed, unauthed);
